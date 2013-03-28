08bd14e720b0ae00a475c63d98eb4276b67ab66c
juancgarcia <juangarcia.info@gmail.com>
2013-03-28 12:34:04 -0400

    adding .gitignore

--- app/js/app.js
+++ app/js/app.js
@@ -4,7 +4,8 @@ require.config({
     jquery: 'vendor/jquery-1.9.1',// //cdnjs.cloudflare.com/ajax/libs/jquery/1.9.1/jquery.min.js
     underscore: 'vendor/underscore-1.4.4',// //cdnjs.cloudflare.com/ajax/libs/underscore.js/1.4.4/underscore-min.js
     backbone: 'vendor/backbone-1.0.0',// //cdnjs.cloudflare.com/ajax/libs/backbone.js/1.0.0/backbone-min.js
-    'backbone.localStorage': 'vendor/backbone.localStorage'
+    //'backbone.localStorage': 'vendor/backbone.localStorage',
+    'backbone.subroute':'vendor/backbone.subroute-0.3.2'
     // //cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/2.1.1/bootstrap.min.js
   },
   shim: {
@@ -18,12 +19,16 @@ require.config({
     'backbone.localStorage': {
       deps: ['backbone'],
       exports: 'Backbone'
+    },
+    'backbone.subroute':{
+      deps: ['backbone']//,
+      //exports: 'Backbone'
     }
   }
 });
 
-require([ 'jquery', 'backbone', 'views/Header', 'views/Home', 'views/List', 'views/About' /*, 'views/Main'*/],
-  function($, Backbone, HeaderView, HomeView, ListView, AboutView/*, MainView*/ ) {
+require([ 'jquery', 'backbone', 'views/Header', 'views/Home', 'views/List', 'views/About', 'backbone.subroute'],
+  function($, Backbone, HeaderView, HomeView, ListView, AboutView, Subroute) {
 
   var Router = Backbone.Router.extend({
     routes: {
